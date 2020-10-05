56705f5e443e585cddd628dff50393e80834b458
adeptex <arteaocho@gmail.com>
2020-10-05 22:34:29 +0200

    Improve JS detection

--- whispers/plugins/javascript.py
+++ whispers/plugins/javascript.py
@@ -6,10 +6,19 @@ from whispers.utils import strip_string
 class Javascript:
     def pairs(self, filepath: Path):
         for line in filepath.open("r").readlines():
-            if "=" in line:
+            if line.count("=") == 1:
                 yield from self.parse_assignment(line)
 
     def parse_assignment(self, line: str):
         key, value = line.split("=")
         value = strip_string(value.replace(";", ""))
-        yield key, value
+        if not self.is_function(value):
+            yield key, value
+
+    def is_function(self, value: str) -> bool:
+        open_brackets = value.count("(")
+        close_brackets = value.count(")")
+        if open_brackets:
+            if open_brackets == close_brackets:
+                return True
+        return False

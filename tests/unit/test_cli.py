1f89447604c610401d644a5fe249055454e01fde
adeptex <arteaocho@gmail.com>
2020-11-06 23:14:23 +0100

    CLI unit tests

---
+++ tests/unit/test_cli.py
@@ -0,0 +1,60 @@
+from argparse import ArgumentParser
+from io import StringIO
+from unittest.mock import patch
+
+import pytest
+
+from whispers.cli import cli, cli_info, cli_parser, parse_args
+from whispers.rules import WhisperRules
+
+from .conftest import does_not_raise, fixture_path
+
+
+def test_cli_parser():
+    assert isinstance(cli_parser(), ArgumentParser)
+
+
+@pytest.mark.parametrize(
+    ("arguments", "expectation"),
+    [
+        ([], {"config": None, "output": None, "rules": "all", "src": None}),
+        (["whispers.json"], {"config": None, "output": None, "rules": "all", "src": "whispers.json"}),
+        (["-c", "whispers.yml"], {"config": "whispers.yml", "output": None, "rules": "all", "src": None}),
+        (["-r", "whis,pers"], {"config": None, "output": None, "rules": "whis,pers", "src": None}),
+        (["-o", "/tmp/whispers"], {"config": None, "output": "/tmp/whispers", "rules": "all", "src": None}),
+    ],
+)
+def test_parse_args(arguments, expectation):
+    args = parse_args(arguments)
+    assert args.config == expectation["config"]
+    assert args.output == expectation["output"]
+    assert args.rules == expectation["rules"]
+    assert args.src == expectation["src"]
+
+
+@pytest.mark.parametrize(
+    ("arguments", "expectation"),
+    [
+        ([], pytest.raises(SystemExit)),
+        (["-v"], pytest.raises(SystemExit)),
+        (["-i"], pytest.raises(SystemExit)),
+        (["-c", "whispers.yml"], pytest.raises(SystemExit)),
+        (["-r", "whis,pers"], pytest.raises(SystemExit)),
+        (["-o", "/tmp/whispers"], pytest.raises(SystemExit)),
+        (["/dev/null/bin"], pytest.raises(FileNotFoundError)),
+        ([f"{fixture_path('hardcoded.json')}"], does_not_raise()),
+    ],
+)
+def test_cli(arguments, expectation):
+    with expectation:
+        assert cli(arguments) is None
+
+
+def test_cli_info():
+    mock_print = StringIO()
+    with patch("sys.stdout", mock_print):
+        cli_info()
+        result = mock_print.getvalue()
+        assert "available rules" in result
+        for rule_id in WhisperRules().rules.keys():
+            assert rule_id in result
--- whispers/cli.py
+++ whispers/cli.py
@@ -8,32 +8,33 @@ from whispers.rules import WhisperRules
 from whispers.utils import format_stdout
 
 
-def whispersArgumentsParser() -> ArgumentParser:
+def cli_parser() -> ArgumentParser:
     args_parser = ArgumentParser("whispers", description=("Identify secrets and dangerous behaviours"))
     args_parser.add_argument("-v", "--version", action="version", version=f"whispers {__version__}")
     args_parser.add_argument("-i", "--info", action="store_true", default=False, help="show extended help and exit")
     args_parser.add_argument("-c", "--config", default=None, help="config file (.yml)")
     args_parser.add_argument("-r", "--rules", default="all", help="comma-separated rule ID list")
     args_parser.add_argument("-o", "--output", help="output file (.yml)")
-    args_parser.add_argument("src", nargs="?", help="source code file or directory")
+    args_parser.add_argument("src", nargs="?", help="target file or directory")
     return args_parser
 
 
 def parse_args(arguments=None) -> Namespace:
-    return whispersArgumentsParser().parse_args(arguments)
+    args, _ = cli_parser().parse_known_args(arguments)
+    return args
 
 
 def cli(arguments=None):
     # Parse CLI arguments
-    args = parse_args()
+    args = parse_args(arguments)
 
     # Show information
     if args.info:
-        exit(info())
+        exit(cli_info())
 
     # Default response
     if not args.src:
-        exit(whispersArgumentsParser().print_help())
+        exit(cli_parser().print_help())
 
     # Clear output file
     if args.output:
@@ -53,10 +54,10 @@ def cli(arguments=None):
     cleanup_log()
 
 
-def info():
+def cli_info():
     rule_ids = list(WhisperRules().rules.keys())
     rule_ids.sort()
-    whispersArgumentsParser().print_help()
+    cli_parser().print_help()
     print("\navailable rules:")
     for rule_id in rule_ids:
         print(f"  {rule_id}")

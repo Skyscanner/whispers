dcf5604c23e35f1f751aedb1189b358e57c9f5f5
adeptex <arteaocho@gmail.com>
2021-08-18 02:37:16 +0200

    Config coverage

--- whispers/plugins/config.py
+++ whispers/plugins/config.py
@@ -1,13 +1,18 @@
 from pathlib import Path
 
+from whispers.utils import strip_string
+
 
 class Config:
     def pairs(self, filepath: Path):
         for line in filepath.open("r").readlines():
+            line = line.strip()
             if "=" not in line:
                 continue
-            if line.index("=") == len(line) - 1:
-                continue
 
-            item = line.strip().split("=", 1)
-            yield item[0], item[1]
+            key, value = line.split("=", 1)
+            key = strip_string(key)
+            value = strip_string(value)
+
+            if value:
+                yield key, value

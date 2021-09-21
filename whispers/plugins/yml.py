e2d3b1de4ddb48168a6b68aadc8db5030488b32b
adeptex <arteaocho@gmail.com>
2021-09-22 01:11:02 +0200

    Remove YAML resolvers for on/off/yes/no

--- whispers/plugins/yml.py
+++ whispers/plugins/yml.py
@@ -3,12 +3,19 @@ from pathlib import Path
 from typing import Iterator
 
 import yaml
+from yaml.resolver import Resolver
 
 from whispers.core.utils import KeyValuePair
 from whispers.plugins.traverse import StructuredDocument
 
 
 class Yml(StructuredDocument):
+    def __init__(self):
+        super().__init__()
+
+        # Remove resolvers for on/off/yes/no
+        list(map(lambda idx: Resolver.yaml_implicit_resolvers.pop(idx, None), "OoYyNn"))
+
     def pairs(self, filepath: Path) -> Iterator[KeyValuePair]:
         def _constructor(loader, tag_suffix, node):
             """This is needed to parse IaC syntax"""
@@ -23,13 +30,17 @@ class Yml(StructuredDocument):
         - Remove comments that start with #
         """
         document = ""
+
         for line in filepath.open("r").readlines():
             if line.startswith("---"):
                 continue
+
             if re.match(r".+(\[)?\{\{.*\}\}(\])?", line):
                 line = line.replace('"', "'")
                 line = line.replace("{{", '"{{').replace("}}", '}}"')
+
             document += line
+
         document = re.sub(r"[<{]%.*?%[}>]", "", document, flags=re.MULTILINE | re.DOTALL)
         document = re.sub(r"^#.*$", "", document)
 
